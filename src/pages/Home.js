8c9aba4b74ad980b5b17600f6bfcb03c81598b2d
MariamElSahhar <me1693@nyu.edu>
2023-11-27 12:49:03 +0400

    basic website

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,13 +21,13 @@ function Home () {
                 </div>
             </header>
             <ProgramsSection />
-            <section className="join">
+            {/* <section className="join">
                 <div className="section-header">
                     <h1 className="take-action">How You Can Get Involved</h1>
                 </div>
                 <div className="section-content">
                     <div className="participate section-item join-item">
-                        <img src="/assets/images/attend-an-event.webp" alt=""></img>
+                        <img src="/assets/images/bb-donate-ca.jpg" alt=""></img>
                         <button>Start a Chapter</button>
                     </div>
                     <div className="join-donate section-item join-item">
@@ -39,7 +39,7 @@ function Home () {
                         <button>Attend an Event</button>
                     </div>
                 </div>
-            </section>
+            </section> */}
             {/* <section className="news">
                 <h1>News</h1>
                 <article></article>
