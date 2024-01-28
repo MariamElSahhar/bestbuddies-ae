6d8d94ea997ee911dc0f0f0334bd91c3b42c176a
MariamElSahhar <me1693@nyu.edu>
2024-01-28 20:43:18 +0400

    bring back react router yayy

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from "react-router-dom";
+// import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 
-import Home from './pages/Home';
-import Programs from './pages/Programs';
-import Involved from './pages/Involved';
-import Media from './pages/Media';
-import Contact from './pages/Contact';
-import Donate from './pages/Donate';
-import FAQ from './pages/FAQ';
-import Friendship from './pages/Programs/Friendship';
-import Leadership from './pages/Programs/Leadership';
-import Employment from './pages/Programs/Employment';
 import About from './pages/About';
 import Buddy from './pages/Get-Involved/Buddy';
+import Contact from './pages/Contact';
 import Chapter from './pages/Get-Involved/Chapter';
-import Mentor from './pages/Get-Involved/Mentor';
+import Donate from './pages/Donate';
 import Employer from './pages/Get-Involved/Employer';
+import Employment from './pages/Programs/Employment';
 import Events from './pages/Get-Involved/Events';
+import FAQ from './pages/FAQ';
+import Friendship from './pages/Programs/Friendship';
+import Home from './pages/Home';
+import Involved from './pages/Involved';
+import Leadership from './pages/Programs/Leadership';
+import Media from './pages/Media';
+import Mentor from './pages/Get-Involved/Mentor';
+import Programs from './pages/Programs';
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="programs" element={<Programs />} />
-          <Route path="get-involved" element={<Involved />} />
-          <Route path="media" element={<Media />} />
-          <Route path="contact-us" element={<Contact />} />
-          <Route path="donate" element={<Donate />} />
-          <Route path="FAQ" element={<FAQ />} />
-          <Route path="friendship" element={<Friendship />} />
-          <Route path="employment" element={<Employment />} />
-          <Route path="leadership" element={<Leadership />} />
           <Route path="become-a-buddy" element={<Buddy />} />
           <Route path="start-a-chapter" element={<Chapter />} />
-          <Route path="become-a-mentor" element={<Mentor />} />
+          <Route path="contact-us" element={<Contact />} />
+          <Route path="donate" element={<Donate />} />
           <Route path="become-an-employer" element={<Employer />} />
+          <Route path="employment" element={<Employment />} />
           <Route path="events" element={<Events />} />
+          <Route path="FAQ" element={<FAQ />} />
+          <Route path="friendship" element={<Friendship />} />
+          <Route path="get-involved" element={<Involved />} />
+          <Route path="leadership" element={<Leadership />} />
+          <Route path="media" element={<Media />} />
+          <Route path="become-a-mentor" element={<Mentor />} />
+          <Route path="programs" element={<Programs />} />
         </Routes>
         <ScrollToTop />
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
